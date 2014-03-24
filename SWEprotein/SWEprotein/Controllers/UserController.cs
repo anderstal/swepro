9b2974ed8a14b3702ce9dc231574259f12835cf3
anderstal <oskaranderstal@gmail.com>
2014-03-24 14:48:32 +0100

    Redigera sitt konto+uppdaterat accountcontroller

--- SWEprotein/SWEprotein/Controllers/UserController.cs
+++ SWEprotein/SWEprotein/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SWEprotein.Models;
+using WebMatrix.WebData;
 
 namespace SWEprotein.Controllers
 {
@@ -10,11 +12,35 @@ namespace SWEprotein.Controllers
     {
         //
         // GET: /User/
+        private readonly DataClasses1DataContext _db = new DataClasses1DataContext();
 
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult EditUserInfo()
+        {
+            var singleUserInfo = _db.UserInfos.FirstOrDefault(c => c.iID == WebSecurity.CurrentUserId);
+            return View(singleUserInfo);
+        }
+
+        [HttpPost]
+        public ActionResult EditUserInfo(UserInfo userInfo)
+        {
+            foreach (UserInfo or in _db.UserInfos.Where(c => c.iID == userInfo.iID))
+            {
+                or.Adress = userInfo.Adress;
+                or.PostalNumber = userInfo.PostalNumber;
+                or.sEmail = userInfo.sEmail;
+                or.sTelephone = userInfo.sTelephone;
+                or.sCity = userInfo.sCity;
+
+                _db.SubmitChanges();
+                return RedirectToAction("Index", "User");
+            }
+            return View();
+
+        }
     }
 }
